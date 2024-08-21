b81c8c415102b37f1f1ef2a525252f3259fecaa2
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-20 20:13:15 -0400

    falta menos

--- src/assets/components/Semaforo.jsx
+++ src/assets/components/Semaforo.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react"
+
 export function Semaforo () {
+
+    const [color, setColor] = useState("verde")
+
+    function changeColor(){
+        switch(color){
+            case "rojo":
+                setColor("amarillo");
+                break;
+            case "amarillo":
+                setColor("verde");
+                break;
+            case "verde":
+                setColor("rojo");
+                break;
+                default:
+                break;
+
+        }
+    }
     return(
         <div className="d-flex flex-column align-items-center">
             <div className="soporte-semaforo border border-dark">
             </div>
             <div className="cuerpo-semaforo rounded-3 border border-dark d-flex flex-column align-items-center">
-                <div className="luz-roja rounded-circle"></div>
-                <div className="luz-amarilla rounded-circle"></div>
-                <div className="luz-verde rounded-circle"></div>
+                <div onClick={()=> setColor("rojo")} className={`luz-roja rounded-circle ${color=="rojo"? "luz_encendida": " "}`}></div>
+                <div onClick={()=> setColor("amarillo")} className={`luz-amarilla rounded-circle ${color=="amarillo"? "luz_encendida": " "}`}></div>
+                <div onClick={()=> setColor("verde")} className={`luz-verde rounded-circle ${color=="verde"? "luz_encendida": " "}`}></div>
             </div>
-
+            <button onClick={()=> changeColor("color")} className={`btn btn-secondary`}>Cambio de Luz</button>
+            <button onClick={()=> purpleColor("color")} className={`btnPurple btn`}>Luz Púrpura!</button>
         </div>
     )
 }
