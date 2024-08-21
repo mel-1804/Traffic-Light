683571e228d1e145712c476fa6d9e4845b1f1f5b
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-21 18:09:34 -0400

    semaforo terminado

--- src/assets/components/Semaforo.jsx
+++ src/assets/components/Semaforo.jsx
@@ -20,17 +20,26 @@ export function Semaforo () {
 
         }
     }
+
+    const[nuevaLuz, setNuevaLuz] = useState(false)
+
     return(
         <div className="d-flex flex-column align-items-center">
-            <div className="soporte-semaforo border border-dark">
+            <div className="semaforoSup d-flex align-items-center">
+                <div className="soporteSemaforo"></div>
+                <div className="cuerpoPurpura rounded-end d-flex flex-column align-items-center">
+                    <div className={`luzPurpura rounded-circle ${nuevaLuz? "luzEncendida": " "}`}></div>
+                    <i class="vaca fa-solid fa-cow"></i>
+                </div>
             </div>
-            <div className="cuerpo-semaforo rounded-3 border border-dark d-flex flex-column align-items-center">
-                <div onClick={()=> setColor("rojo")} className={`luz-roja rounded-circle ${color=="rojo"? "luz_encendida": " "}`}></div>
-                <div onClick={()=> setColor("amarillo")} className={`luz-amarilla rounded-circle ${color=="amarillo"? "luz_encendida": " "}`}></div>
-                <div onClick={()=> setColor("verde")} className={`luz-verde rounded-circle ${color=="verde"? "luz_encendida": " "}`}></div>
+            <div className="cuerpoSemaforo rounded-3 d-flex flex-column align-items-center">
+                <div onClick={()=> setColor("rojo")} className={`luzRoja rounded-circle ${color=="rojo"? "luzEncendida": " "}`}></div>
+                <div onClick={()=> setColor("amarillo")} className={`luzAmarilla rounded-circle ${color=="amarillo"? "luzEncendida": " "}`}></div>
+                <div onClick={()=> setColor("verde")} className={`luzVerde rounded-circle ${color=="verde"? "luzEncendida": " "}`}></div>
             </div>
             <button onClick={()=> changeColor("color")} className={`btn btn-secondary`}>Cambio de Luz</button>
-            <button onClick={()=> purpleColor("color")} className={`btnPurple btn`}>Luz Púrpura!</button>
+            <button onClick={()=> setNuevaLuz(!nuevaLuz)} className="btnPurple btn">Luz Púrpura!!</button>
+
         </div>
     )
 }
